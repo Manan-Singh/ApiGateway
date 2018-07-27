c300c3960909d3fd7d16889fef8a16de176e4491
manan <manan.singh@stonybrook.edu>
2018-07-26 21:06:03 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftTrading API Gateway
+
+This is a small Java Spring Boot app works as an entry point to the SwiftTrading REST API. 
---
+++ src/main/java/app/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    public String home() {
+        return "index";
+    }
+}
